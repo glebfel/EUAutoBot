1426bcd23de8d7a6131f2a709fa98053c8aa693e
glebfel <felyust@list.ru>
2022-09-15 22:53:44 +0300

    add basic logic for bot

--- telegram_bot/handlers/client.py
+++ telegram_bot/handlers/client.py
@@ -1,6 +1,6 @@
 from aiogram import types, Dispatcher
-from aiogram.types import ParseMode
-from aiogram.utils.markdown import text, italic
+from aiogram.types import ParseMode, CallbackQuery
+from aiogram.utils.markdown import text, italic, bold
 from telegram_bot.init_bot import dp
 from telegram_bot.inline_keyboard import start_markup
 
@@ -8,11 +8,35 @@ from telegram_bot.inline_keyboard import start_markup
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.answer(text('Я могу посчитать стоимость авто 🚘 из Германии "под ключ" в РФ.',
-                             f'\n\nДля расчета нужна {(italic("ссылка"))} на конкретный авто.',
-                             f'\n\nЧто бы начать, нажмите кнопку ниже 👇'),
-                        reply_markup=start_markup,
-                        parse_mode=ParseMode.MARKDOWN)
+                              f'\n\nДля расчета нужна {(italic("ссылка"))} на конкретный авто.',
+                              f'\n\nЧто бы начать, нажмите кнопку ниже 👇'),
+                         reply_markup=start_markup,
+                         parse_mode=ParseMode.MARKDOWN)
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.answer(text(bold("Доступные команды:"),
+                              "/start - возврат к началу ⏭",
+                              "/moderate - режим модератора 🖥 (доступен только администратору бота)",
+                              sep="\n"
+                              ),
+                         parse_mode=ParseMode.MARKDOWN)
+
+
+@dp.callback_query_handler(text="calculate")
+async def process_calculate_button(callback: CallbackQuery):
+    await callback.message.answer("Пожалуйста, пришлите ссылку на интересуемое авто 🚙🔍")
+
+
+@dp.message_handler()
+async def process_other_commands(message: types.Message):
+    await message.answer(text("Не могу распознать введенную команду 🧐❗",
+                              "\nДля просмотра доступных команд воспользуйтесь /help 🧾"),
+                         parse_mode=ParseMode.MARKDOWN)
 
 
 def register_client_handlers(dp: Dispatcher):
     dp.register_message_handler(process_start_command)
+    dp.register_message_handler(process_help_command)
+    dp.register_message_handler(process_other_commands)
--- telegram_bot/inline_keyboard.py
+++ telegram_bot/inline_keyboard.py
@@ -1,12 +1,8 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from telegram_bot.init_bot import dp
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 
 start_btn = InlineKeyboardButton('Рассчитать стоимость', callback_data='calculate')
 start_markup = InlineKeyboardMarkup().add(start_btn)
 
 
-@dp.message_handler(text="calculate")
-async def process_calculate_button(callback: CallbackQuery):
-    # logic for calculation(parser and etc.)
-    await callback.message.answer('')
-    pass
+

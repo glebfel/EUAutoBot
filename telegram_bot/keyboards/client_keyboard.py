9bdb15502ed735bd78f7b1529ff8a76003214def
glebfel <felyust@list.ru>
2022-09-19 01:02:19 +0300

    add link to send a message

--- telegram_bot/keyboards/client_keyboard.py
+++ telegram_bot/keyboards/client_keyboard.py
@@ -11,7 +11,7 @@ error_markup = InlineKeyboardMarkup(row_width=2).add(cancel_btn, retry_btn)
 
 # markup for car info output
 call_btn = InlineKeyboardButton('Позвонить 📞', callback_data='call')
-message_btn = InlineKeyboardButton('Написать 📱', callback_data='message')
+message_btn = InlineKeyboardButton('Написать 📱', url='https://t.me/makarusan')
 another_car_btn = InlineKeyboardButton('Подсчитать другою машину 🚙', callback_data='another')
 get_more_info_btn = InlineKeyboardButton('Узнать стоимость оформления ЭПТС и СБКТС на этот автомобиль',
                                          callback_data='info')

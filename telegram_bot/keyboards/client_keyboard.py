c356079237b0a8393c65a22d62b93669c7f5d2d4
glebfel <felyust@list.ru>
2022-09-19 02:13:20 +0300

    fix markup

--- telegram_bot/keyboards/client_keyboard.py
+++ telegram_bot/keyboards/client_keyboard.py
@@ -14,7 +14,7 @@ call_btn = InlineKeyboardButton('Позвонить 📞', callback_data='call')
 message_btn = InlineKeyboardButton('Написать 📱', url='https://t.me/makarusan')
 another_car_btn = InlineKeyboardButton('Подсчитать другою машину 🚙', callback_data='another')
 get_more_info_btn = InlineKeyboardButton('Узнать стоимость оформления ЭПТС и СБКТС на этот автомобиль',
-                                         callback_data='https://t.me/makarusan')
+                                         url='https://t.me/makarusan')
 car_info_markup = InlineKeyboardMarkup(row_width=2).add(call_btn, message_btn).add(get_more_info_btn).add(
     another_car_btn)
 

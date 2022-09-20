4c243f801b5c845e94466f313e188a77e3026875
glebfel <felyust@list.ru>
2022-09-21 01:21:18 +0300

    fix markup

--- telegram_bot/keyboards/client_keyboard.py
+++ telegram_bot/keyboards/client_keyboard.py
@@ -7,7 +7,7 @@ start_markup = InlineKeyboardMarkup().add(start_btn)
 # markup for car info output
 call_btn = InlineKeyboardButton('Позвонить 📞', callback_data='call')
 message_btn = InlineKeyboardButton('Написать в WhatsApp 📲', url='https://api.whatsapp.com/send/?phone=79252205782')
-another_car_btn = InlineKeyboardButton('Подсчитать другою машину 🚙', callback_data='another')
+another_car_btn = InlineKeyboardButton('Подсчитать другую машину 🚙', callback_data='another')
 get_more_info_btn = InlineKeyboardButton('Узнать стоимость оформления ЭПТС и СБКТС на этот автомобиль',
                                          url='https://api.whatsapp.com/send/?phone=79252205782')
 car_info_markup = InlineKeyboardMarkup(row_width=2).add(call_btn, message_btn).add(get_more_info_btn).add(

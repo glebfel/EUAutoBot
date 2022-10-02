17dfa0e055e898bffa92f4e019b15fb6a6891d2f
glebfel <felyust@list.ru>
2022-10-02 16:54:35 +0300

    made logs rewritable after each restart

--- core/logger.py
+++ core/logger.py
@@ -4,5 +4,5 @@ from loguru import logger
 
 logger.remove(0)
 logger.add(sys.stderr, colorize=True)
-logger.add("logs.log", level="INFO", rotation="100 MB", compression="zip")
+logger.add('logs.log', level='INFO', rotation='100 MB', compression='zip', mode='w')
 custom_logger = logger.bind(specific=True)
